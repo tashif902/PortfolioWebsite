1ef3f864a37de661c7a467139a73b4175a178204
tashif902 <tashif902@gmail.com>
2025-05-05 23:23:59 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const buttons = document.querySelectorAll('button');
+buttons.forEach(button => {
+    button.addEventListener('mouseover', () => {
+        button.style.transform = 'scale(1.1)';
+    });
+
+    button.addEventListener('mouseout', () => {
+        button.style.transform = 'scale(1)';
+    });
+});
